2b244f0c89f824e88ab6ee280198e50eebe7a03b
github NamiKikuchi <github k.n.luvivi@gmail.com>
2020-05-25 18:18:48 +0900

    Shop_TagTableSeederを作成

---
+++ database/seeds/Shop_TagTableSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class Shop_TagTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      $param=[
+        'shop_id' => 1,
+        'tag_id' => 1,
+      ];
+      DB::table('shop_tag')->insert($param);
+
+      $param=[
+        'shop_id' => 1,
+        'tag_id' => 3,
+      ];
+      DB::table('shop_tag')->insert($param);
+
+      $param=[
+        'shop_id' => 2,
+        'tag_id' => 1,
+      ];
+      DB::table('shop_tag')->insert($param);
+
+      $param=[
+        'shop_id' => 2,
+        'tag_id' => 4,
+      ];
+      DB::table('shop_tag')->insert($param);
+
+      $param=[
+        'shop_id' => 2,
+        'tag_id' => 6,
+      ];
+      DB::table('shop_tag')->insert($param);
+    }
+}
