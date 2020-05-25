ac6a5d3c9293876ca926bb9ed83a9e924b6bed23
github NamiKikuchi <github k.n.luvivi@gmail.com>
2020-05-25 12:36:10 +0900

    shop.php変数名を変更

--- app/Shop.php
+++ app/Shop.php
@@ -6,24 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Shop extends Model
 {
-    public function scopeLunchMinPrice($query, $n){
-      return $query->where('lunch_price', '>=', $n);
+    public function scopeLunchMinPrice($query, $price){
+      return $query->where('lunch_price', '>=', $price);
     }
 
-    public function scopeLunchMaxPrice($query, $n){
-      return $query->where('lunch_price', '<=', $n);
+    public function scopeLunchMaxPrice($query, $price){
+      return $query->where('lunch_price', '<=', $price);
     }
 
-    public function scopeDinnerMinPrice($query, $n){
-      return $query->where('dinner_price', '>=', $n);
+    public function scopeDinnerMinPrice($query, $price){
+      return $query->where('dinner_price', '>=', $price);
     }
 
-    public function scopeDinnerMaxPrice($query, $n){
-      return $query->where('dinner_price', '<=', $n);
+    public function scopeDinnerMaxPrice($query, $price){
+      return $query->where('dinner_price', '<=', $price);
     }
 
-    public function scopeStation($query, $n){
-      return $query->where('station', $n);
+    public function scopeStation($query, $serch_station){
+      return $query->where('station', $serch_station);
     }
 
     public function tags(){
