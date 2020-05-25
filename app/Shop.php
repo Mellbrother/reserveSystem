df53a73413ac12946e7e89d31238494918cba973
github NamiKikuchi <github k.n.luvivi@gmail.com>
2020-05-25 11:49:05 +0900

    shopモデルni

--- app/Shop.php
+++ app/Shop.php
@@ -6,5 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Shop extends Model
 {
-    //
+    public function scopeLunchMinPrice($query, $n){
+      return $query->where('lunch_price', '>=', $n);
+    }
+
+    public function scopeLunchMaxPrice($query, $n){
+      return $query->where('lunch_price', '<=', $n);
+    }
+
+    public function scopeDinnerMinPrice($query, $n){
+      return $query->where('dinner_price', '>=', $n);
+    }
+
+    public function scopeDinnerMaxPrice($query, $n){
+      return $query->where('dinner_price', '<=', $n);
+    }
+
+    public function scopeStation($query, $n){
+      return $query->where('station', $n);
+    }
+
+    public function tags(){
+      //belongsToManyの処理
+      return $this->belongsToMany('App\Tag', 'shop_tag', 'tag_id', 'shop_id')
+    }
 }
