7a3bc3cbdebcbe641bf67f1d1d9b24f18eb5427b
sakorisi <rt.232931372357@gmail.com>
2020-05-26 18:27:01 +0900

    adminページのUIを微修正

--- resources/views/admin/user_edit.blade.php
+++ resources/views/admin/user_edit.blade.php
@@ -4,7 +4,7 @@
     @if (count($errors) > 0)
     <div>
         <ul>
-            @foreach ($error->all() as $error)
+            @foreach ($errors->all() as $error)
                 <li>{{$error}}</li>
             @endforeach
         </ul>
@@ -12,6 +12,11 @@
     @endif
 
     <form action="/admin/{{$id}}/userUpdate" method="post">
+<!--rules次第ではこのif文は不要かも？-->
+    @if($form == null)
+    <p>このユーザーは存在していないか、削除されています。</p><br>
+    <a href="/admin/{{$id}}/home">管理者ホームに戻る</a><br>
+    @else
     <table>
         @csrf
         <input type="hidden" name="id" value="{{$form->id}}">
@@ -19,11 +24,15 @@
         <tr><th>name: </tr><td><input type="text" name="name" value="{{$form->email}}"></td></tr>
         <tr><th></th><td><input type="submit" value="編集"></td><tr>
     </table>
+    @endif
     </form>
 
     <form action="/admin/{{$id}}/userDelete" method="post">
+<!--上に同じ？-->
+    @if($form != null)
         @csrf
         <input type="submit" value="削除">
+    @endif
     </form>
 @endsection
         
