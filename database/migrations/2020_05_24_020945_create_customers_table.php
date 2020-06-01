ea522b5ece7dfac5a8ab1f77380d8132369719c4
github NamiKikuchi <github k.n.luvivi@gmail.com>
2020-06-02 01:07:04 +0900

    各ユーザーのマイグレーションファイルのidをincrementsに変更

--- database/migrations/2020_05_24_020945_create_customers_table.php
+++ database/migrations/2020_05_24_020945_create_customers_table.php
@@ -14,7 +14,7 @@ class CreateCustomersTable extends Migration
     public function up()
     {
         Schema::create('customers', function (Blueprint $table) {
-          $table->id();
+          $table->increments('id');
           $table->string('name');
           $table->string('email')->unique();
           $table->timestamp('email_verified_at')->nullable();
