fcdc72cd082cee546ded7443f0f4f2fd832c7f87
AoyamaKoji <kouzi.tennis.head@gmail.com>
2020-06-01 13:16:33 +0900

    ジャンル選択、時間のエラーなし

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -7,6 +7,9 @@ use App\Shop;
 use App\Reserve;
 use App\Tag;
 use App\Station;
+use Datetime;
+use DateInterval;
+use Dateperiod;
 
 class CustomerController extends Controller
 {
@@ -29,14 +32,14 @@ class CustomerController extends Controller
 
     public function showSearchResult(Request $request, $id)
     {
+
+      $tags = Tag::all();
+      $stations = Station::all();
+
       if ($request->name != null){
         $shops = Shop::where('name', $request->name)->get();
       } else {
-
         $shops = Shop::station($request->station);
-        if($request->tag != ""){
-          $shops = Tag::where('name', $request->tag)->first()->shops();
-        }
 
         if($request->price != ""){
           preg_match('/(\d+)~(\d+)/', $request->price, $result);
@@ -45,54 +48,63 @@ class CustomerController extends Controller
           $shops = $shops->lunchMinPrice($min_price)
                       ->lunchMaxPrice($max_price);
         }
+
         $shops = $shops->get();
+        //
+        // if($request->tag != ""){
+        //   for($i=0; $i<count($tags); $i++){
+        //     $shops = Tag::wherein('name', $shops->tag)->get();
+        //   }
+        // }
+        // else {
+        //   $shops = $shops->get();
+        // }
       }
 
-        $param = [
-          'name' => $request->name,
-          'shops' => $shops,
-          'id' => $id
-         ];
+
+
+      $param = [
+        'name' => $request->name,
+        'shops' => $shops,
+        'id' => $id,
+        'tags' => $tags,
+        'stations' => $stations
+       ];
         return view('customer.search_result', $param);
      }
 
     public function showShopDetail($id, $shop_id)
     {
         $shop = Shop::where('id', $shop_id)->first();
-        return view('customer.shop_detail', ['shop' => $shop]);
+        $param = [
+          'id' => $id,
+          'shop_id' => $shop_id,
+          'shop' => $shop
+        ];
+        return view('customer.shop_detail', $param);
     }
 
-    public function showReservePage(Request $request, $id, $shop_id)
+    public function showReservePage($id, $shop_id)
     {
-        $opens = Shop::find($shop_id)->open;
-        $closes = Shop::find($shop_id)->close;
-        preg_match('/(\d+):(\d+)/', $opens, $result_open);
-        $hour_open = $result_open[1];
-
-        preg_match('/(\d+):(\d+)/', $closes, $result_close);
-        $hour_close = $result_close[1];
-
-        $start = new DateTime();
-        $end = new DateTime();
-
-        $start->setTime($hour_open);
-        $end->setTime($hour_close);
-
-        $date_interval = new DateInterval('PT1H');
-
-        $date_period = new DatePeriod($start, $date_interval, $end);
-
-        foreach ($date_period as $key->$val){
-            $time = ["date_period" => $date_period];
-        }
+        $shop = Shop::where('id', $shop_id)->first();
+        // $open = $shop->open;
+        // $close = $shop->close;
+        //
+        //
+        // $start = new DateTime($open);
+        // $end =  new DateTime($close);
+        //
+        // $date_interval = new DateInterval('PT1H');
+        //
+        // $date_period = new DatePeriod($start, $date_interval, $end);
 
         $param = [
           "id" => $id,
           "shop_id" => $shop_id,
-          "date_period" => "$date_period"
+          // "date_period" => $date_period
         ];
 
-        return view('customer.reserve', $param, $time);
+        return view('customer.reserve', $param);
     }
 
     public function reserve(Request $request, $id, $shop_id)
--- resources/views/customer/search_result.blade.php
+++ resources/views/customer/search_result.blade.php
@@ -32,8 +32,135 @@
   @endforeach
 
 </table>
+
+<hr>
+
+<form class="content" action="searchResult" method="post">
+  @csrf
+  <div style="display:inline-flex">
+    <div>
+      <select name="station">
+      @foreach($stations as $station)
+        <option value="{{$station->station}}">{{$station->station}}</option>
+      @endforeach
+      </select>
+    </div>
+
+    <div>
+      <select id="ms" name="tag" multiple="multiple">
+        @foreach($tags as $tag)
+          <option value="{{$tag->name}}">{{$tag->name}}</option>
+        @endforeach
+      </select>
+    </div>
+
+{{--
+    <div>
+      <select id="ms" name="tag" multiple="multiple">
+        @foreach($tags as $tag)
+          <option value="{{$tag->name}}">{{$tag->name}}</option>
+        @endforeach
+      </select>
+    </div>
+--}}
+
+    <div>
+      <select name="price">
+        <option value="">¥こだわらない</option>
+        <option value="500~1000">500~1000円</option>
+        <option value="1001~1500円">1001~1500円</option>
+        <option value="1501~2000円">1501~2000円</option>
+        <option value="2001~3000円">2001~3000円</option>
+        <option value="3001~4000円">3001~4000円</option>
+        <option value="4001~5000円">4001~5000円</option>
+        <option value="5001~7000円">5001~7000円</option>
+        <option value="7001~10000円">7001~10000円</option>
+        <option value="10001~15000円">10001~15000円</option>
+        <option value="15001~20000円">15001~20000円</option>
+        <option value="20001~30000円">20001~30000円</option>
+      </select>
+    </div>
+
+    <br>
+
+    <div class="detail">
+      <button type="submit" class="btn btn-primary">再検索</button>
+    </div>
+  </div>
+
+</form>
 @endsection
 
 @section('footer')
 copyright 2020 GroupA.
 @endsection
+
+
+
+
+
+
+{{--
+  <div >
+    <select id = "ms" class="btn btn-danger dropdown-toggle" name="tag" multiple="multiple">
+      <optgroup label="すべての項目">
+        @foreach($tags as $tag)
+          <option type="checkbox" value="{{$tag->name}}">{{$tag->name}}</option>
+        @endforeach
+      </optgroup>
+    </select>
+
+  </div>
+
+    <div class="dropdown">
+      <select class="btn btn-danger dropdown-toggle" name="tag">
+        <option value="">ジャンルすべて</option>
+      @foreach($tags as $tag)
+        <option value="{{$tag->name}}">{{$tag->name}}</option>
+      @endforeach
+      </select>
+
+    </div>
+
+
+    <div>
+      <button type="button" class = "btn btn-danger dropdown-toggle">
+        <p>ジャンル</p>
+        <div class="default">
+          <table width="180px" id="genre">
+          @foreach($tags as $tags)
+            <tr>
+              <td wigth="50px">{{$tags->name}}</td>
+              <td><input type="checkbox" name="tag[]"
+                value="{{$tags->name}}"></td>
+            </tr>
+          @endforeach
+          </table>
+        </div>
+      </div>
+
+      <div class="dropdown">
+        <button type="button"
+          class="btn btn-danger dropdown-toggle"
+          data-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false">
+          ジャンル
+        </button>
+        <div class="dropdown-menu" aria-labelledby="dropdown1">
+          <div class="default">
+            <table width="180px" id="genre">
+            @foreach($tags as $tags)
+              <tr>
+                <td wigth="50px">{{$tags->name}}</td>
+                <td><input type="checkbox" name="tag[]"
+                  value="{{$tags->name}}"></td>
+              </tr>
+            @endforeach
+            </table>
+          </div>
+        </div>
+      </div>
+
+
+--}}
